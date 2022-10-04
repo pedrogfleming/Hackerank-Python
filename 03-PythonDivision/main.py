c1732c1e3bb00dd372a753103b9f17a5cc6e11b6
pedrogfleming <pedrogfleming@gmail.com>
2022-10-04 01:57:39 -0300

    Python basic arithmetic operators and division

---
+++ 03-PythonDivision/main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    # SOLUTION 1
+    a, b = [int(input()) for _ in range(2)]
+    for op in ['//', '/']:
+      print(eval(f'a{op}b'))
+    # a = int(input())
+    # b = int(input())
+    # SOLUTION 2
+    # # Int division
+    # print(a // b)
+    # # Float division
+    # print(a / b)
+
+    # My Solution
+    # print(int(a/b))
+    # print(float(a/b))
